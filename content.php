962d34bb9b6650c3e2d6bfa04d8a9c38c25bc519
TheXmaster123456 <Belillimounir123@gmail.com>
2018-05-10 00:34:24 +0100

    Creating Single.php and content.php and content-single.php

--- content.php
+++ content.php
@@ -40,12 +40,28 @@
 				<div  id="top" class="callbacks_container wow fadeInUp" data-wow-delay="0.5s">
 					<ul class="rslides" id="slider3">
 						<li>
+                        
 							<div class="banner-inf">
 								<h3>soluta nobis est eligendi cumque</h3>
 								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis, ante interdum vehicula pretium, dui enim porta
 								lectus, non euismod tortor ante eu libero</p>
 								<a href="single.html">See More</a>
+                                <div class="blog-post">
+ <h2 class="blog-post-itle"><?php the_title(); ?></h2>
+ <p class="blog-post-meta"><?php the_date(); ?> par <a
+href="#"><?php the_author(); ?></a></p>
+ <?php the_content(); ?>
+ </div>
 							</div>
+                       
+ <h4>Archives</h4> <ol class="list-unstyled"> <?php
+wp_get_archives( 'type=monthly' ); ?> </ol> 
+<h4>About</h4> <p><?php the_author_meta( 'description' ); ?>
+</p> 
+ 
+
+<!-- /.blog-post -->
+
 						</li>
 						<li>
 							<div class="banner-inf">
