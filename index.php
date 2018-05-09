a3bee9fe040a857e7547d04a107822bdfffec240
TheXmaster123456 <Belillimounir123@gmail.com>
2018-05-09 22:12:31 +0100

    Creating Css style and Index page

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 		function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- //for-mobile-apps -->
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php bloginfo('template_directory'); ?>/css/bootstrap.css" rel="stylesheet">
+<link href="<?php bloginfo('template_directory'); ?>/css/style.css" rel="stylesheet">
 <!-- js -->
 <script src="js/jquery-1.11.1.min.js"></script>
 <!-- //js -->
